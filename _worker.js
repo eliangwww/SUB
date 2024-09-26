31cbfa9ee9bcd0fbd465d78aad1178afa9f7b086
eliangwww <96296131+eliangwww@users.noreply.github.com>
2024-09-27 00:25:57 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -16,20 +16,6 @@ let cacheTTL = 24 ;//小时，缓存时长
 let MainData = `
 vless://d342d11e-d424-4583-b36e-524ab1f0afa4@20.48.5.180:80?path=Telegram%F0%9F%87%A8%F0%9F%87%B3+%40WangCai_8+%2F%3Fed%3D2048&security=none&encryption=none&host=a.ssll.gay&type=ws#%F0%9F%87%AF%F0%9F%87%B5JP 🈲请勿测速
 `
-fetch('https://raw.githubusercontent.com/eliangwww/wangcai/main/data/ipdb_data.txt')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.text();  // 读取文本内容
-  })
-  .then(data => {
-    let MainData = data;  // 将获取的文本内容赋值给变量 MainData
-      // 输出数据
-  })
-  .catch(error => {
-    console.error('Error fetching the data:', error);  // 捕获并显示错误
-  });
 
 
 let urls = [];
